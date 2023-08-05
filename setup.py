f8160432796dcb806c69eccf2f57b1cc10cbb3c6
Marcos Paulo <marcosppaivap@gmail.com>
2023-08-05 11:35:25 +0100

    setup.py for install packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlops',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A short description of the project.',
+    author='Marcos Pereira',
+    license='',
+)
