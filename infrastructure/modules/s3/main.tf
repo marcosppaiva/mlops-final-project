0701572cc691e8f0d23436daf8a63203dcb3f2b3
Marcos Paulo <marcosppaivap@gmail.com>
2023-08-17 17:11:56 +0100

    add subdirectories and force_destroy

--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -1,11 +1,18 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
+  force_destroy = true
 }
 
-resource "aws_s3_object" "object" {
+resource "aws_s3_object" "object_1" {
   bucket = aws_s3_bucket.s3_bucket.id
-  key    = "data/"
+  key    = "data/processed/"
 }
+
+resource "aws_s3_object" "object_2" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  key    = "data/raw/"
+}
+
 
 output "bucket" {
   value = aws_s3_bucket.s3_bucket.bucket
